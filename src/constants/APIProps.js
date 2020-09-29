f1a77ca0781e48cd5fd7c36593e0ec6ed1447eb4
Melih Güleç <melih.gulec14@gmail.com>
2020-09-29 21:26:52 +0300

    Added constants.

---
+++ src/constants/APIProps.js
@@ -0,0 +1,7 @@
+const lang = "TR";       // available: tr, kr, de, hi, ru and more @ https://openweathermap.org/current#multi
+const units = "metric";  // available: metric and imperial.
+
+export default {
+    lang, 
+    units
+};
---
+++ src/constants/Colors.js
@@ -0,0 +1,9 @@
+const main = '#363062';
+const mainLight = '#4d4c7d';
+const palePurple = '#d5fdff';
+
+export default {
+    main, 
+    mainLight, 
+    palePurple
+};
