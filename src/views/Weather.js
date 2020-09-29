e166563c39609883a53d1dfdeff341578a6cd4f1
Melih Güleç <melih.gulec14@gmail.com>
2020-09-29 21:30:57 +0300

    Added Home and Weather views

---
+++ src/views/Weather.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function Weather(){
+    return(
+        <View>
+            <Text>Weather Page</Text>
+        </View>
+    );
+}
+
+export default Weather;
